38a317324ea1b35dc21d6f7c839dfff2ee55baf3
Worawut Chusri <at3biz@gmail.com>
2026-06-13 20:42:44 +0700

    fix: update CORS middleware configuration to specify allowed origin and credentials

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const categoryRoutes = require('./routes/category');
 // Middleware to log requests
 app.use(moregan('dev'));
 app.use(express.json({limit:'50mb'}));
-app.use(cors());
+//app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 
 
