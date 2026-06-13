54917a3f38cd944a37ec3f2e4503315c7914e4a6
Worawut Chusri <at3biz@gmail.com>
2026-06-13 21:07:21 +0700

    fix: enable CORS middleware for request handling

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const categoryRoutes = require('./routes/category');
 // Middleware to log requests
 app.use(moregan('dev'));
 app.use(express.json({limit:'50mb'}));
-//app.use(cors());
+app.use(cors());
 
 
 
@@ -23,11 +23,11 @@ app.use(express.json({limit:'50mb'}));
 //   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 // }));
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://ecom2024-ver1-api.vercel.app'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'https://ecom2024-ver1-api.vercel.app'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 
 
 // Auto load routes
